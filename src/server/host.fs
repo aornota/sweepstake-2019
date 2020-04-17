d5c2bb4116b91bc95a7549373efb99d4d8d8ca0e
aornota <djnarration@gmail>
2020-04-17 15:17:23 +0100

    Attempt to get working on Azure as netcoreapp3.1.

--- src/server/host.fs
+++ src/server/host.fs
@@ -1,9 +1,9 @@
 ﻿module Aornota.Sweepstake2019.Server.Host
 
 open Aornota.Sweepstake2019.Common.IfDebug
+open Aornota.Sweepstake2019.Common.Literals
 open Aornota.Sweepstake2019.Common.UnitsOfMeasure
 
-open Aornota.Sweepstake2019.Common.Literals
 open Aornota.Sweepstake2019.Server.Agents
 open Aornota.Sweepstake2019.Server.Agents.Broadcaster
 open Aornota.Sweepstake2019.Server.Agents.Connections
@@ -11,48 +11,18 @@ open Aornota.Sweepstake2019.Server.Agents.ConsoleLogger
 open Aornota.Sweepstake2019.Server.Agents.Persistence
 open Aornota.Sweepstake2019.Server.Agents.Ticker
 open Aornota.Sweepstake2019.Server.DefaultData
-open Aornota.Sweepstake2019.Server.WsMiddleware
+open Aornota.Sweepstake2019.Server.Startup
 
 open System
-open System.IO
 
 open Microsoft.AspNetCore
-open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.DependencyInjection
-
-open Giraffe
 
 let [<Literal>] private SECONDS_PER_TICK = 1<second/tick>
 
 let private log category = (Host, category) |> consoleLogger.Log
 
 let private serverStarted = DateTimeOffset.UtcNow
-
-let private publicPath =
-    let publicPath = Path.Combine ("..", "ui/public") |> Path.GetFullPath // e.g. when served via webpack-dev-server
-    if Directory.Exists publicPath then publicPath else "public" |> Path.GetFullPath // e.g. when published/deployed
-
-let private indexPath = Path.Combine (publicPath, "index.html")
-
-let private webApp : HttpFunc -> Http.HttpContext -> HttpFuncResult = choose [ route "/" >=> htmlFile indexPath ]
-
-let private configureApp (app:IApplicationBuilder) =
-    app.UseGiraffe webApp |> ignore
-    app.UseStaticFiles () |> ignore
-    app.UseWebSockets () |> ignore
-    app.UseMiddleware<WsMiddleware> () |> ignore
-
-let private configureServices (services:IServiceCollection) = services.AddGiraffe () |> ignore
-
-let private builder = WebHost.CreateDefaultBuilder ()
-
-builder.UseWebRoot publicPath |> ignore
-builder.UseContentRoot publicPath |> ignore
-builder.Configure (Action<IApplicationBuilder> configureApp) |> ignore
-// TODO-NMB-LOW: Suppress ASP.Net Core logging (since can get mixed up with ConsoleLogger output, i.e. since Console not thread-safe)?... builder.ConfigureLogging (...) |> ignore
-builder.ConfigureServices configureServices |> ignore
-builder.UseUrls (sprintf "http://0.0.0.0:%i/" WS_PORT) |> ignore
 
 "starting ConsoleLogger agent" |> Info |> log // note: will be logged as IgnoredInput (since ConsoleLogger agent not yet started) - but this is fine since consoleLogger.Log is not blocking
 ifDebug logEverythingExceptVerboseAndTicker logWarningsAndWorseOnly |> consoleLogger.Start
@@ -94,6 +64,13 @@ serverStarted |> connections.Start
 
 "ready" |> Info |> log
 
-let private host = builder.Build ()
-
-host.Run ()
+WebHost
+    .CreateDefaultBuilder()
+    .UseWebRoot(publicPath)
+    .UseContentRoot(publicPath)
+    .UseUrls(sprintf "http://0.0.0.0:%i/" WS_PORT)
+    (* TODO-NMB-LOW: Suppress ASP.Net Core logging (since can get mixed up with ConsoleLogger output, i.e. since Console not thread-safe)?...
+    .ConfigureLogging(...) *)
+    .UseStartup<Startup>()
+    .Build()
+    .Run()
